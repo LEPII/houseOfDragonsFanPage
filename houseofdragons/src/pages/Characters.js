68cbc90cfa5507d8c82923fb2b48c465b67e834d
Luis Perez <lperezdev843@gmail.com>
2023-03-04 20:13:31 -0500

    finishing touches

--- houseofdragons/src/pages/Characters.js
+++ houseofdragons/src/pages/Characters.js
@@ -2,15 +2,19 @@ import React, { useRef, useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import data from "../data.json";
 import "../style/characters.css";
+import { BsHandIndex } from "react-icons/bs";
 
 const Characters = () => {
   const [hodCharacter] = useState(data.characters);
   const [width, setWidth] = useState(0);
 
-  const carousel = useRef();
+  const carousel = useRef(null);
+
 
   useEffect(() => {
     setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    const helper = document.querySelector(".helper_icon");
+    helper.classList.add("helper_icon_load")
   }, []);
 
   return (
@@ -18,36 +22,39 @@ const Characters = () => {
       <article className="characters_text">
         <h3>
           01<span> CHOOSE YOUR MONARCH </span>
-        </h3>
+        </h3><BsHandIndex className="helper_icon" />
+        
         <motion.div
           ref={carousel}
           className="characters_carousel"
           whileHover={{ scale: 1.01 }}
         >
           <motion.div
+          
             className="characters_inner_carousel"
             drag="x"
             dragConstraints={{ right: 0, left: -width }}
           >
-            {hodCharacter && hodCharacter.map((player, index) => {
-              return (
-                <motion.div className="characters_titles" key={index}>
-                  <h4>{player.name}</h4>
-                  <h6>of House: {player.house}</h6>
-                  <h6>Homeland: {player.region}</h6>
-                  <p>{player.description}</p>
-                  <img
-                    src={player.portrait}
-                    alt={player.name}
-                    title={player.name}
-                    width="300"
-                    height="300"
-                    loading="lazy"
-                    className="characters_image"
-                  />
-                </motion.div>
-              );
-            })}
+            {hodCharacter &&
+              hodCharacter.map((player, index) => {
+                return (
+                  <motion.div className="characters_titles" key={index}>
+                    <h4>{player.name}</h4>
+                    <h6>of House: {player.house}</h6>
+                    <h6>Homeland: {player.region}</h6>
+                    <p>{player.description}</p>
+                    <img
+                      src={player.portrait}
+                      alt={player.name}
+                      title={player.name}
+                      width="300"
+                      height="300"
+                      loading="lazy"
+                      className="characters_image"
+                    />
+                  </motion.div>
+                );
+              })}
           </motion.div>
         </motion.div>
       </article>
--- houseofdragons/src/pages/Home.js
+++ houseofdragons/src/pages/Home.js
@@ -4,6 +4,7 @@ import "../style/home.css";
 import { motion } from "framer-motion";
 
 const Home = () => {
+
   return (
     <section className="home_container">
       <p>What is this breif, mortal life, if not the pursuit of legacy?</p>
@@ -11,17 +12,18 @@ const Home = () => {
         House of Dragons <br />
       </h1>
       <h2>Fans Page</h2>
-      
+
       <p>
         Explore the monarchs that keeps the kingdoms at bay with their dragons,
         the lands they rule and sometimes burn to conquer, and the groups that
         keeps everything together from remembering the world's memories to
         protecting us from the long night...
       </p>
-  <motion.button whileHover={{ scale: 1.25 }}> 
-      <Link to="/characters" className="explore_button">
-      EXPLORE 
-      </Link></motion.button>
+      <motion.button whileHover={{ scale: 1.25 }}>
+        <Link to="/characters" className="explore_button">
+          EXPLORE
+        </Link>
+      </motion.button>
     </section>
   );
 };
